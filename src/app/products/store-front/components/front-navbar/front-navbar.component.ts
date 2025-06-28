1e7c704d20d3c98f41276dfcda1fb6052120ae45
marco-soria <marco.soriaa7@gmail.com>
2025-06-28 10:03:04 -0500

    initial routing for store

---
+++ src/app/products/store-front/components/front-navbar/front-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-front-navbar',
+  imports: [],
+  templateUrl: './front-navbar.component.html'
+})
+export class FrontNavbarComponent {
+
+}
---
+++ src/app/products/store-front/pages/product-page/product-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  imports: [],
+  templateUrl: './product-page.component.html'
+})
+export class ProductPageComponent {
+
+}
